13c0a36bab1e881885b2573757fbdfb7e04ad5fe
Qonus <94901082+Qonus@users.noreply.github.com>
2025-05-01 00:04:37 +0500

    [Add] title, description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import SetsPage from "@/components/pages/sets-page";
+import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
+  const t = await getTranslations("Home");
   return (
-    <SetsPage />
+    <div className="w-full">
+      <div className="flex mb-10 p-4">
+        <div className="card m-auto max-w-150 flex-col gap-5">
+
+          <h1 className="title m-auto text-2xl">{t("title")}</h1>
+          <p className="gray m-auto text-xl">{t("description")}</p>
+        </div>
+      </div>
+
+      <SetsPage />
+    </div>
   )
 }
