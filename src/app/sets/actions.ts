5b84881d6b508151566d2699bb9c283289a17691
Qonus <94901082+Qonus@users.noreply.github.com>
2025-05-03 13:41:06 +0500

    [Fix] missing await

--- src/app/sets/actions.ts
+++ src/app/sets/actions.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export async function getSetByID(id: string) {
     try {
-        const res = await axios.get(`${getBaseUrl()}/api/sets/${id}`);
+        const res = await axios.get(`${await getBaseUrl()}/api/sets/${id}`);
         return res.data;
     } catch {
         return null;
@@ -13,13 +13,13 @@ export async function getSetByID(id: string) {
 }
 
 export async function deleteSet(id: string) {
-    const res = await axios.delete(`${getBaseUrl()}/api/sets/${id}`);
+    const res = await axios.delete(`${await getBaseUrl()}/api/sets/${id}`);
     return res.data;
 }
 
 export async function getSets(userId?: string) {
     try {
-        const sets = await axios.get(`${getBaseUrl()}/api/sets?${userId && `userid=${userId}`}`);
+        const sets = await axios.get(`${await getBaseUrl()}/api/sets?${userId && `userid=${userId}`}`);
         return sets.data;
     } catch {
         return null;
