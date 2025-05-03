3a4f39deddb6e72d1716380b3da1d1bffe5c8f49
Qonus <94901082+Qonus@users.noreply.github.com>
2025-05-03 10:42:50 +0500

    [Fix] backface issue

--- src/components/review-flashcards.tsx
+++ src/components/review-flashcards.tsx
@@ -25,7 +25,7 @@ export default function ReviewFlashcards({
 
     const variants = {
         enter: {
-            originZ: 500,
+            originZ: 300,
             rotateY: direction * -90,
             opacity: 0.2
         },
@@ -36,48 +36,50 @@ export default function ReviewFlashcards({
             rotateX: isFront ? 0 : -180,
         },
         exit: (dir: number) => ({
-            originZ: 500,
+            originZ: 300,
             rotateY: dir * 90,
-            opacity: 0.2
+            opacity: 0.2,
+            transition: { duration: 0.1 }
         })
     };
 
     return (
-        <div className="flex flex-col gap-5 perspective-midrange">
-            <h1 className="title text-xl relative m-auto">{current + 1}/{cards.length}</h1>
-            <AnimatePresence
-                mode="popLayout"
-                custom={direction}
-            >
-                <motion.div
-                    key={current}
-                    variants={variants}
-                    initial="enter"
-                    animate="center"
-                    exit="exit"
-                    transition={{ duration: 0.5, type: "spring" }}
-                    className={`bg-card overflow-hidden rounded-3xl glow h-70 max-w-140 text-[clamp(1.5rem,5cqw,2rem)] leading-tight cursor-pointer m-auto w-full transform-3d`}
-                    onClick={() => {
-                        setIsFront(!isFront);
-                    }}
+        <div className="flex flex-col gap-5">
+            <h1 className="title text-xl m-auto">{current + 1}/{cards.length}</h1>
+            <div className="perspective-midrange">
+                <AnimatePresence
+                    mode="popLayout"
+                    custom={direction}
                 >
-                    <div
-                        className={`size-full p-7 flex absolute rotate-y-0 ${isFront ? "" : "hidden"}`}
+                    <motion.div
+                        key={current}
+                        variants={variants}
+                        initial="enter"
+                        animate="center"
+                        exit="exit"
+                        transition={{ duration: 0.5, type: "spring" }}
+                        className={`relative bg-card rounded-3xl glow h-70 max-w-140 text-[clamp(1.5rem,5cqw,2rem)] leadintg-tight cursor-pointer m-auto w-full transform-3d`}
+                        onClick={() => {
+                            setIsFront(!isFront);
+                        }}
                     >
-                        <p className="m-auto">
-                            {cards[current].front}
-                        </p>
-                    </div>
-                    <div
-                        className={`size-full p-7 flex absolute rotate-x-180 ${isFront ? "hidden" : ""}`}
-                    // initial={{ rotateX: 180 }}
-                    >
-                        <p className="m-auto">
-                            {cards[current].back}
-                        </p>
-                    </div>
-                </motion.div>
-            </AnimatePresence>
+                        <div
+                            className="size-full p-7 flex absolute backface-hidden rotate-y-0"
+                        >
+                            <p className="m-auto">
+                                {cards[current].front}
+                            </p>
+                        </div>
+                        <div
+                            className="size-full p-7 flex absolute backface-hidden [transform:rotateX(180deg)]"
+                        >
+                            <p className="m-auto">
+                                {cards[current].back}
+                            </p>
+                        </div>
+                    </motion.div>
+                </AnimatePresence>
+            </div>
             <div className="sticky max-w-200 m-auto bottom-0 w-full text-2xl flex justify-between p-5">
                 <button onClick={() => nav(-1)}
                     className={cn("outline", current === 0 ? "dark:brightness-40" : "")}>
